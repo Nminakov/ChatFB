e1bee8bb319fd2a5883642ac9057468c41bd1ec0
Nminakov <cleon96@mail.ru>
2020-10-17 09:44:23 +0300

    Initial Commit

---
+++ ChatFB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatFB
+//
+//  Created by Никита on 17.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
